965ca569ae9d23751de3ecf2d3a180aa11922111
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2023-05-07 09:14:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import mpltable
+import plotly.figure_factory as ff
+import plotly.express as px
+import plotly.subplots as sp
+import plotly.io as pio
 
 dados = pd.read_excel("Vendas.xlsx")
 
+# Agrupar os dados por 'ID Loja' e somar o 'Valor Final'
 resultado = dados.groupby('ID Loja')['Valor Final'].sum()
-df_resultado = pd.DataFrame(resultado)
 
-titulo = 'Soma do Valor Final por ID Loja'
-cores = ('#F5F5F5', '#D3D3D3')
-table = mpltable.table(df_resultado, titulo=titulo, colWidths=[0.2, 0.2], cellColours=[cores]*len(df_resultado), cellLoc='center')
+# Criar um novo dataframe com o resultado
+df_resultado = pd.DataFrame(resultado).reset_index()
 
-# Exibir a tabela na tela
-fig, ax = plt.subplots()
-ax.axis('off')
-table.auto_set_font_size(False)
-table.set_fontsize(14)
-plt.show()
+# Altera o renderizador para jupyterlab
+pio.renderers.default = 'jupyterlab'
 
-# resultado.plot(kind='bar', figsize=(6,4))
-# plt.xlabel('ID Loja')
-# plt.ylabel('Valor Final')
-# plt.title('Soma do Valor Final por ID Loja')
-# plt.show()
+# Cria a tabela com plotly.figure_factory
+fig2 = ff.create_table(df_resultado)
 
-print(resultado)
+# Cria o gráfico de barras com plotly.express
+fig1 = px.bar(df_resultado, x='ID Loja', y='Valor Final', title='Valor Final por ID Loja')
+
+# Cria um objeto subplot com 2 linhas e 1 coluna
+fig = sp.make_subplots(rows=2, cols=1)
+
+# Adiciona o gráfico de barras à primeira linha
+fig.add_trace(fig1.data[0], row=1, col=1)
+fig.update_layout(title=fig1.layout.title.text, yaxis_title=fig1.layout.yaxis.title.text)
+
+# Adiciona a tabela à segunda linha
+fig.add_trace(fig2.data[0], row=2, col=1)
+
+# Exibe o objeto subplot no navegador
+fig.show()
